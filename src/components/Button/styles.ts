da2b8c32c340b053e9890a10c62931dec46fb519
Fabricio Bedin <fabricioobedin@gmail.com>
2020-08-23 14:31:37 -0300

    adding fonts and creating SignIn layout with Input and Button components

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #ed6707;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+`;
+export const ButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #eee;
+  font-size: 17px;
+`;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
-
 import logo from '../../assets/logo_upf.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logo} />
+
+      <Title>Faça seu login</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
